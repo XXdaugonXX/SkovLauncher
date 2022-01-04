8c6c58f4ea66fbe084e83463e8546cf1b33f8e30
XXdaugonXX <fredrik@severinsen.nu>
2022-01-04 17:29:49 +0100

    V2.0.1
    Small Changes

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -49,7 +49,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/dscalzi/ZayvielLauncher/releases/download/v${info.version}/zayviellauncher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : ''}.dmg`
+                    info.darwindownload = `https://github.com/XXdaugonXX/ZayvielLauncher/releases/download/v${info.version}/zayviellauncher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : ''}.dmg`
                     showUpdateUI(info)
                 }
                 
